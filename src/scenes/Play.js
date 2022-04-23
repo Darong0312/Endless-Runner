6f49d8655e425ec347353a17f34315682a990408
Darong Li <dli97@ucsc.edu>
2022-04-23 02:03:14 -0700

    update with jump, frames

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,50 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 
+    // adding img
+    preload(){
+        this.load.image('BabyOwl','./assets/BabyOwl.png');
+        this.load.image('OwlFlutter','./assets/OwlFlutter.png');
+        this.load.image('field','./assets/field.png');
+        this.load.image('ground','./assets/ground.png');
+    }
+
     create(){
-        this.field = this.add.tileSprite(0,0,640,480).setOrigin(0,0);
+        // setting background
+        this.field = this.add.tileSprite(0,0,800,600,'field').setOrigin(0,0);
+
+        
+        // init ground
+        platforms = this.physics.add.staticGroup();
+        platforms.create(400,700, 'ground').setScale(2).refreshBody();
+
+        // init owl
+        owl = this.physics.add.sprite(game.config.width/15, game.config.height - borderUISize - borderPadding*10,'BabyOwl');
+
+        // setting jump key to space bar
+        keySpace = this.input.keyboard.addKey(32);
+
+        this.gameOver = false;
+
+        // adding collide with owl and platform
+        this.physics.add.collider(owl,platforms);
+    }
+
+    update(){
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keySpace)) {
+            this.scene.restart();
+        }
+
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keySpace)) {
+            this.scene.start("menuScene");
+        }
+
+        // give the owl an upward velocity when space bar is hit
+        if(Phaser.Input.Keyboard.JustDown(keySpace)){
+            owl.setVelocityY(-330);
+        }
+
+        // background move speed
+        this.field.tilePositionX -= 3;
     }
 }
