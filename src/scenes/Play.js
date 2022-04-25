9ccd089a03169345bbcbd7ac888fdb5b94d6ef6d
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-04-25 11:49:17 -0700

    change platform

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,3 +1,9 @@
+let gameOptions = {
+    platformStartSpeed: 350,
+    spawnRange: [100, 350],
+    platformSizeRange: [50, 250],
+}
+
 class Play extends Phaser.Scene{
     constructor(){
         super("playScene");
@@ -12,6 +18,7 @@ class Play extends Phaser.Scene{
         this.load.image('plat','./assets/platform.png');
     }
 
+
     create(){
         // setting background
         this.field = this.add.tileSprite(0,0,800,600,'field').setOrigin(0,0);
@@ -19,20 +26,36 @@ class Play extends Phaser.Scene{
         this.jump = false;
 
         // init ground
-        platforms = this.physics.add.staticGroup();
-        platforms.create(300,600, 'plat').setScale(2).refreshBody();
+        //platforms = this.physics.add.staticGroup();
+        //platforms.create(300,600, 'plat').setScale(2).refreshBody();
+
+        this.platformGroup = this.add.group({
+ 
+            // once a platform is removed, it's added to the pool
+            removeCallback: function(platform){
+                platform.scene.platformPool.add(platform)
+            }
+        });
+ 
+        // pool
+        this.platformPool = this.add.group({
+ 
+            // once a platform is removed from the pool, it's added to the active platforms group
+            removeCallback: function(platform){
+                platform.scene.platformGroup.add(platform)
+            }
+        });
+
+        this.addPlatform(game.config.width, game.config.width / 2);
 
         // init owl
         owl = this.physics.add.sprite(game.config.width/15, game.config.height - borderUISize - borderPadding*10,'BabyOwl');
-        //this.owl = new owl(this,game.config.width/15, game.config.height - borderUISize - borderPadding*10,'BabyOwl').setOrigin(0.5, 0);
 
         // setting jump key to space bar
         keySpace = this.input.keyboard.addKey(32);
 
         // adding collide with owl and platform
-        this.physics.add.collider(owl,platforms);
-
-        //this.physics.add.collider(owl, this.platformGroup);
+        this.physics.add.collider(owl,this.platformGroup);
         
         //setting left and right key.
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -47,6 +70,26 @@ class Play extends Phaser.Scene{
 
     }
 
+    addPlatform(platformWidth, posX){
+        let platform;
+        if(this.platformPool.getLength()){
+            console.log("inside if");
+            platform = this.platformPool.getFirst();
+            platform.x = posX;
+            platform.active = true;
+            platform.visible = true;
+            this.platformPool.remove(platform);
+        }
+        else{
+            console.log("inside else");
+            platform = this.physics.add.sprite(posX, game.config.height * 0.8, "plat");
+            platform.setImmovable(true);
+            platform.setVelocityX(gameOptions.platformStartSpeed * -1);
+            this.platformGroup.add(platform);
+        }
+        platform.displayWidth = platformWidth;
+        this.nextPlatformDistance = Phaser.Math.Between(gameOptions.spawnRange[0], gameOptions.spawnRange[1]);
+    }
 
     update(){
         owl.setVelocityX(0);
@@ -57,6 +100,22 @@ class Play extends Phaser.Scene{
 
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keySpace)) {
             this.scene.start("menuScene");
+        }
+
+        let minDistance = game.config.width;
+        this.platformGroup.getChildren().forEach(function(platform){
+            let platformDistance = game.config.width - platform.x - platform.displayWidth / 2;
+            minDistance = Math.min(minDistance, platformDistance);
+            if(platform.x < - platform.displayWidth / 2){
+                this.platformGroup.killAndHide(platform);
+                this.platformGroup.remove(platform);
+            }
+        }, this);
+ 
+        // adding new platforms
+        if(minDistance > this.nextPlatformDistance){
+            var nextPlatformWidth = Phaser.Math.Between(gameOptions.platformSizeRange[0], gameOptions.platformSizeRange[1]);
+            this.addPlatform(nextPlatformWidth, game.config.width + nextPlatformWidth / 2);
         }
 
         //let the owl move
