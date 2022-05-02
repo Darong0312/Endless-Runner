e070a7638a149d5b97865bfb83916b64e1ed756c
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-05-01 17:22:24 -0700

    added controls scene

--- src/scenes/teach.js
+++ src/scenes/teach.js
@@ -1,15 +1,15 @@
-class Menu extends Phaser.Scene {
+class teach extends Phaser.Scene {
     constructor() {
-        super("menuScene");
+        super("teachScene");
     }
 
     preload(){
-        this.load.image('OwlMenu','./assets/OwlMenu.png');
+        this.load.image('Owlteach','./assets/owl controls.png');
         this.load.audio('select','./assets/select.wav');
     }
     create() {
         // show menu image
-        this.add.image(game.config.width/2, game.config.height/2,'OwlMenu');
+        this.add.image(game.config.width/2, game.config.height/2,'Owlteach');
         // define keys
         keySpace = this.input.keyboard.addKey(32);
     }
@@ -17,7 +17,7 @@ class Menu extends Phaser.Scene {
     update(){
         if(Phaser.Input.Keyboard.JustDown(keySpace)){
             this.sound.play('select');
-            this.scene.start("teachScene");
+            this.scene.start("playScene");
             console.log("save");
         }
     }
