56e56d98d7700c3bd1948a406516228df633661b
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-05-02 20:43:32 -0700

    added some comments

--- src/main.js
+++ src/main.js
@@ -35,7 +35,9 @@ We implemented the random generator which generate the platforms, the foxes, and
 This was interesting because we were encounter platforms falling in the first place, which involved with the physics.
 We are proud of able to get thw random generators to work together. 
 We learned from the platform random generator, and implement our own hawk generator.
-
+We countered a bug that it keeps saying that something is not defined.
+We spent some time on it and found out that the position of codes matters in Java Script. When you define anything, you should always place it at the beginning.
+Not like c or c++, you can define a global variable and use it anywhere.
 // art blocks
 
 */
